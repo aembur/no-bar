536adb6f6cfd050df5b29d43c53b62f00e777715
qrystalz@gmail.com <qrystalz@gmail.com>
2022-09-01 16:32:14 +0100

    Fix mod info version

--- src/main/java/com/aembr/nobar/NoBar.java
+++ src/main/java/com/aembr/nobar/NoBar.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class NoBar {
     public static final String MODID = "nobar";
     public static final String NAME = "NoBar";
-    public static final String VERSION = "1.12.2-0.1";
+    public static final String VERSION = "1.12.2-0.1.2";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
