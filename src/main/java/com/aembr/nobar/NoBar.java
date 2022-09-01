4270ead43ddf64ee84270887fbebd14aba415e1d
qrystalz@gmail.com <qrystalz@gmail.com>
2022-09-01 14:07:35 +0100

    Set up mod template

--- src/main/java/com/aembr/nobar/NoBar.java
+++ src/main/java/com/aembr/nobar/NoBar.java
@@ -1,18 +1,17 @@
-package com.example.examplemod;
+package com.aembr.nobar;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = NoBar.MODID, name = NoBar.NAME, version = NoBar.VERSION)
+public class NoBar
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "nobar";
+    public static final String NAME = "NoBar";
+    public static final String VERSION = "0.1";
 
     private static Logger logger;
 
@@ -25,7 +24,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 }
